d4ab2a25c002eea40072926794434ede66956dc5
Jean-Charles SOTTAS <jc.sottas@gmail.com>
2019-05-29 13:51:29 +0200

    Ajout sécurité pour le Lambda

--- Simulation/main.c
+++ Simulation/main.c
@@ -25,7 +25,10 @@ int main(int argc, const char * argv[]) {
 //    FILE* F1 = fopen("Temps1.txt", "w+");
     for (i = 0; i<L.size; i++) {
         //printf("%d \n",L.tab[i]);
-        simulationMM10(F1,L.tab[i]);
+        if(L.tab[i]>=N)
+            fprintf(F1, "%d inf inf\n",L.tab[i]);
+        else
+            simulationMM10(F1,L.tab[i]);
     }
     fclose(F1);
      printf("======== NMM1 ========\n");
@@ -36,7 +39,11 @@ int main(int argc, const char * argv[]) {
 //    FILE* F2 = fopen("Temps2.txt", "w+");
     for (i = 0; i<L.size; i++) {
 //        printf("%d \n",L.tab[i]);
-        simulationMM1(F2,L.tab[i]);
+        if(L.tab[i]>=N)
+            fprintf(F2, "%d inf inf\n",L.tab[i]);
+        else{
+//            simulationMM1(F2,L.tab[i]);
+        }
     }
     fclose(F2);
      printf("======= MM10min =======\n");
@@ -45,7 +52,11 @@ int main(int argc, const char * argv[]) {
 //    FILE* F3 = fopen("/Users/jean-charles/Desktop/Simulation/Simulation/Simulation/Temps3.txt", "w+");
 //    FILE* F3 = fopen("Temps3.txt", "w+");
     for (i = 0; i<L.size; i++) {
-        simulationMM10min(F3,L.tab[i]);
+        if(L.tab[i]>=N)
+            fprintf(F3, "%d inf inf\n",L.tab[i]);
+        else{
+//            simulationMM10min(F3,L.tab[i]);
+        }
     }
     fclose(F3);
     freeL(L);
